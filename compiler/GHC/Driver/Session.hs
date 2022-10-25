e901b6b4c0bf3f58480e8b9543d751ade4114637
David Knothe <dknothe314@me.com>
2022-10-25 09:18:26 +0200

    Implement Or Patterns (Proposal 0522)
    This commit introduces a language extension, `OrPatterns`, as described in proposal 0522.
    
    It extends the syntax by the production
    `pat -> (one of pat1, ..., patk)`.
    The or-pattern `pat` succeeds iff one of the patterns `pat1`, ..., `patk` succeed, in this order.
    
    Currently, or-patterns cannot bind variables. They are still of great use as they discourage the use of wildcard patterns in favour of writing out all "default" cases explicitly:
    
    ```
    isIrrefutableHsPat pat = case pat of
      ...
      (one of WildPat{}, VarPat{}, LazyPat{})
          = True
      (one of PArrPat{}, ConPatIn{}, LitPat{}, NPat{}, NPlusKPat{}, ListPat{})
          = False
    ```
    
    This makes code safer where data types are extended now and then - just like GHC's `Pat` in the example when adding the new `OrPat` constructor. This would be catched by `-fwarn-incomplete-patterns`, but not when a wildcard pattern was used.
    
    - Update submodule haddock.
    
    stuff
    
    Implement empty one of
    
    Prohibit TyApps
    
    Remove unused
    
    update submodule haddock
    
    Update tests
    
    Parser.y

--- compiler/GHC/Driver/Session.hs
+++ compiler/GHC/Driver/Session.hs
@@ -2698,6 +2698,7 @@ xFlagsDeps = [
   depFlagSpec' "NullaryTypeClasses"           LangExt.NullaryTypeClasses
     (deprecatedForExtension "MultiParamTypeClasses"),
   flagSpec "NumDecimals"                      LangExt.NumDecimals,
+  flagSpec "OrPatterns"                       LangExt.OrPatterns,
   depFlagSpecOp "OverlappingInstances"        LangExt.OverlappingInstances
     setOverlappingInsts
     "instead use per-instance pragmas OVERLAPPING/OVERLAPPABLE/OVERLAPS",
